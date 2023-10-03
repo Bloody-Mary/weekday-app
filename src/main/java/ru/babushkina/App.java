73b122b90b4758852362fcc36e4336f02942dbd6
Bloody-Mary <marianelalieben@gmail.com>
2023-10-03 18:25:31 +0300

    feature: 03-10-2023 18:24 method main and method translateToRussian were created

--- src/main/java/ru/babushkina/App.java
+++ src/main/java/ru/babushkina/App.java
@@ -1,13 +1,36 @@
 package ru.babushkina;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.text.DateFormatSymbols;
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {       Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the day of the week in English: ");
+            String dayOfWeek = sc.nextLine().toLowerCase();
+            String russianDayOfWeek = translateToRussian(dayOfWeek);
+            System.out.println("The day of the week in English: " + russianDayOfWeek);
+    }
+
+    private static String translateToRussian(String dayOfWeek) {
+        switch (dayOfWeek) {
+            case "monday":
+                return "понедельник";
+            case "tuesday":
+                return "вторник";
+            case "wednesday":
+                return "среда";
+            case "thursday":
+                return "четверг";
+            case "friday":
+                return "пятница";
+            case "saturday":
+                return "суббота";
+            case "sunday":
+                return "воскресенье";
+            default:
+                return "неизвестный день";
+        }
     }
 }
